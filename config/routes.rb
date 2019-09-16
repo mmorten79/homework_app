370666109cc4c023aae587b75d4f00cbfc631830
Michael Morten <mikemorten1979@gmail.com>
2019-09-16 00:07:29 -0500

    testing adding questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'mmorten', to: 'static_pages#mmorten', as: 'mmorten'
   get 'aboutMe', to: 'static_pages#aboutMe', as: 'aboutMe'
-  get 'changes', to: 'static_pages#changes', as: 'changes'
+  get 'questions', to: 'static_pages#questions', as: 'questions'
   root to: redirect('/mmorten', status: 302)
   # get 'static_pages/intro'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
