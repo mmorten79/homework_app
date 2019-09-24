333db977b8794de9b2bad60fde22068b6d7e2b13
Michael Morten <mikemorten1979@gmail.com>
2019-09-24 17:37:57 -0500

    Completed and working

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,16 +4,45 @@ class StaticPagesController < ApplicationController
   def mmorten
     respond_to do |format|
       format.html { render :mmorten }
-    end
+      end
    end
 
    def aboutMe
      respond_to do |format|
-      format.html { render :aboutMe}
+      format.html { render :aboutMe }
      end
-end
+  end
 
-def questions
+   
+
+    def form_completed
+       required = [:firstname, :lastname, :answer, :student_classification, :mail_address ] 
+       form_complete = true 
+      required.each do |f|
+        if params.has_key? f and not params[f].blank?
+          #do nothing 
+        else
+          form_complete = false
+        end
+      end
+        if form_complete
+          form_status_msg = "Thank you for your time"
+        else
+          form_status_msg = "Make sure to fill-in all areas"
+        end
+        respond_to do |format|
+          format.html { render :studentinfo, locals: {status_msg: form_status_msg, feedback: params }} 
+        end
+      end
+
+        def studentinfo
+          respond_to do |format|
+          format.html { render :studentinfo, locals: {feedback: {}} }
+          end
+        end
+      
+      
+  def questions
   #variable
   @q = 'Here is a questions for you: Why did the chicken cross the road?'  
   #array
@@ -25,6 +54,7 @@ def questions
   #functions
     respond_to do |format|
       format.html { render :questions, locals: { genQuestion: genQuestion } }
+    
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  post 'studentinfo', to: 'static_pages#form_completed', as: 'form_completed'
   get 'mmorten', to: 'static_pages#mmorten', as: 'mmorten'
   get 'aboutMe', to: 'static_pages#aboutMe', as: 'aboutMe'
   get 'questions', to: 'static_pages#questions', as: 'questions'
+  get 'studentinfo', to: 'static_pages#studentinfo', as: 'studentinfo'
   root to: 'static_pages#mmorten' 
   # root to: redirect('/mmorten', status: 302)
   # get 'static_pages/intro'
